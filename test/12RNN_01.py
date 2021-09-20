0a41565fa7d2984a4eb5037cc643f899b91c15b1
shengrihui <1120058252@qq.com>
2021-09-20 20:43:12 +0800

    RNN RNNcell

---
+++ test/12RNN_01.py
@@ -0,0 +1,66 @@
+# -*- coding: utf-8 -*-
+"""
+Created on  2021/9/19 18:07
+
+@author: shengrihui
+"""
+
+# 使用RNNC、ell
+
+import torch
+
+# 设置参数
+batch_size = 1
+# 序列长度，相当于有多少个x输入，一个单词当中有多少个字母
+# 每次循环拿出此时刻所有的输入
+seq_len = 3
+# 输入的维度，one-hot向量的维度=seq_len*input_size
+input_size = 4
+hidden_size = 2
+
+# 需要指定输入和隐藏的维度
+cell = torch.nn.RNNCell(input_size=input_size, hidden_size=hidden_size)
+
+# （seq_len,batch_size,input_size)
+# 循环的时候遍历seqlen，取出(batch_size,input_size)
+dataset = torch.randn(seq_len, batch_size, input_size)
+# print(dataset)
+# tensor([[[-0.5446,  1.4580, -0.2285, -0.9541]],
+#         [[-0.9974,  0.0500, -1.4568,  0.4851]],
+#         [[-0.3668, -1.7975, -2.3452, -0.1734]]])
+
+# c初始隐藏层设为全零，(batch_size,hidden_size)
+hidden = torch.zeros(batch_size, hidden_size)
+# print(hidden)
+# # tensor([[0., 0.]])
+
+for idx, input in enumerate(dataset):
+    print('=' * 20, idx, '=' * 20)
+    print('input :', input)
+    print('input size:', input.shape)
+
+    # input=(batch_size,input_size)
+    # -> (batch_size,hidden_size)
+    # +
+    # hidden=(batch_size,hidden_size)
+    # ->(batch_size,hidden_size)
+    # ->
+    # output(hidden)=(batch_size,hidden_size)
+    hidden = cell(input, hidden)
+    print('output/hidden :', hidden)
+    print('putput/hidden size:', hidden.shape)
+    # ==================== 0 ====================
+    # input : tensor([[-1.0720, -1.0083,  1.0325,  0.7219]])
+    # input size: torch.Size([1, 4])
+    # output/hidden : tensor([[-0.6823, -0.8587]], grad_fn=<TanhBackward>)
+    # putput/hidden size: torch.Size([1, 2])
+    # ==================== 1 ====================
+    # input : tensor([[-1.6405,  0.0827, -0.5267,  1.1621]])
+    # input size: torch.Size([1, 4])
+    # output/hidden : tensor([[ 0.6336, -0.8969]], grad_fn=<TanhBackward>)
+    # putput/hidden size: torch.Size([1, 2])
+    # ==================== 2 ====================
+    # input : tensor([[-0.8617,  1.0453, -1.4648,  2.7871]])
+    # input size: torch.Size([1, 4])
+    # output/hidden : tensor([[-0.2993, -0.9475]], grad_fn=<TanhBackward>)
+    # putput/hidden size: torch.Size([1, 2])
---
+++ test/12RNN_02.py
@@ -0,0 +1,99 @@
+# -*- coding: utf-8 -*-
+"""
+Created on  2021/9/19 18:39
+
+@author: shengrihui
+"""
+
+# 使用RNNCell解决 hello 到 ohlol
+
+import torch
+
+input_size = 4
+hidden_size = 4
+batch_size = 1  # 因为只有一个样本
+
+idx2char = ['e', 'h', 'l', 'o']
+x_data = [1, 0, 2, 2, 3]  # 戴表idx2char的下标，所以是 hello
+y_data = [3, 1, 2, 3, 2]  # ohlol
+
+# 建立noe-hot向量
+# 先建立one-hot字典，然后根据x_data的值作为下标选取one-hot
+one_hot_lookup = [[1, 0, 0, 0],
+                  [0, 1, 0, 0],
+                  [0, 0, 1, 0],
+                  [0, 0, 0, 1]]
+x_one_hot = [one_hot_lookup[x] for x in x_data]
+# print(x_one_hot)
+# [[0, 1, 0, 0], [1, 0, 0, 0], [0, 0, 1, 0], [0, 0, 1, 0], [0, 0, 0, 1]]
+
+"""
+inputs=torch.tensor(x_one_hot)
+print(inputs)
+tensor([[0, 1, 0, 0],
+        [1, 0, 0, 0],
+        [0, 0, 1, 0],
+        [0, 0, 1, 0],
+        [0, 0, 0, 1]])
+print(inputs.shape)
+torch.Size([5, 4])
+"""
+# 现在的x_one_hot是(seq_len,input_size)的，
+# 但我们还需要有batch_size这一维度，所以要view
+inputs = torch.Tensor(x_one_hot).view(-1, batch_size, input_size)
+# print(inputs)
+# tensor([[[0, 1, 0, 0]],
+#         [[1, 0, 0, 0]],
+#         [[0, 0, 1, 0]],
+#         [[0, 0, 1, 0]],
+#         [[0, 0, 0, 1]]])
+# print(inputs.shape)
+# torch.Size([5, 1, 4])
+
+labels = torch.LongTensor(y_data).view(-1, 1)
+# print(labels)
+# print(labels.shape)
+# tensor([[3],
+#         [1],
+#         [2],
+#         [3],
+#         [2]])
+# torch.Size([5, 1])
+
+
+class Model(torch.nn.Module):
+    def __init__(self, input_size, hidden_size, batch_size):
+        super(Model, self).__init__()
+        self.input_size = input_size
+        self.hidden_size = hidden_size
+        self.batch_size = batch_size
+        self.rnncell = torch.nn.RNNCell(input_size=self.input_size, hidden_size=self.hidden_size)
+    
+    def forward(self, input, hidden):
+        return self.rnncell(input, hidden)
+    
+    def init_hidden(self):
+        return torch.zeros(self.batch_size, self.hidden_size)
+
+
+net = Model(input_size, hidden_size, batch_size)
+
+criterion = torch.nn.CrossEntropyLoss()
+optimizer = torch.optim.Adam(net.parameters(), lr=0.1)
+
+for epoch in range(15):
+    loss = 0
+    hidden = net.init_hidden()
+    optimizer.zero_grad()
+    print("Predicted string:", end='')
+    for input, label in zip(inputs, labels):
+        hidden = net(input, hidden)
+        loss += criterion(hidden, label)
+        _, idx = hidden.max(1)
+        print(idx2char[idx], end='')
+    loss.backward()
+    optimizer.step()
+    print(f"  epoch:{epoch + 1},loss:{loss.item()}")
+
+
+
