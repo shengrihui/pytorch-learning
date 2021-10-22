db26b4958f830458aba82163d65f1fe4dbe5668d
shengrihui <1120058252@qq.com>
2021-10-22 21:30:37 +0800

    201022

---
+++ test/12RNN_03_RNN.py
@@ -0,0 +1,37 @@
+# -*- coding: utf-8 -*-
+"""
+Created on  2021/9/20 20:45
+    
+@author: shengrihui
+"""
+
+#使用RNN
+
+import torch
+
+batch_size=1
+seq_len=3
+input_size=4
+hidden_size=2
+num_layers=5
+
+cell=torch.nn.RNN(input_size=input_size,hidden_size=hidden_size,num_layers=num_layers)
+
+inputs=torch.randn(seq_len,batch_size,input_size)
+hidden=torch.zeros(num_layers,batch_size,hidden_size)
+print('inputs',inputs)
+print('inputs.shape',inputs.shape)
+print('hidden',hidden)
+print('hidden.shape',hidden.shape)
+
+output,hidden=cell(inputs,hidden)
+print('out',output)
+print('out.shape',output.shape)
+#out.shape (seq_len,batch_size,hidden_size)
+print('hidden',hidden)
+print('hidden.shape',hidden.shape)
+#hidden.shape (num_layers,batch_size,hidden_size)
+
+a=output[-1][0]
+b=hidden[-1][0]
+print(a,b,a==b)
---
+++ test/12RNN_04_RNN.py
@@ -0,0 +1,46 @@
+# -*- coding: utf-8 -*-
+"""
+Created on  2021/9/20 21:25
+    
+@author: shengrihui
+"""
+
+#使用RNN解决 hello 到 ohlol
+import torch
+
+input_size=4
+hidden_size=5
+batch_size=1
+num_layers=1
+seq_len=5
+
+class model(torch.nn.Module):
+    def __init__(self,input_size,hidden_size,num_layers,batch_size):
+        super(model, self).__init__()
+        self.input_size=input_size
+        self.hidden_size=hidden_size
+        self.num_layers=num_layers
+        self.batch_size=batch_size
+        self.rnn=torch.nn.RNN(input_size=self.input_size,hidden_size=self.hidden_size,num_layers=self.num_layers)
+    def forward(self,input,hidden):
+        return self.rnn(input,hidden)
+    def init_hidden(self):
+        return torch.zeros(self.num_layers,self.batch_size,self.hidden_size)
+
+net=model(input_size=input_size,hidden_size=hidden_size,num_layers=num_layers,batch_size=batch_size)
+criterion=torch.nn.CrossEntropyLoss()
+optimizer=torch.optim.Adam(net.parameters(),lr=0.1)
+
+idx2char = ['e', 'h', 'l', 'o']
+x_data = [1, 0, 2, 2, 3]
+y_data = [3, 1, 2, 3, 2]
+
+one_hot_lookup = [[1, 0, 0, 0],
+                  [0, 1, 0, 0],
+                  [0, 0, 1, 0],
+                  [0, 0, 0, 1]]
+x_one_hot = [one_hot_lookup[x] for x in x_data]
+
+inputs=torch.Tensor(x_one_hot).view(seq_len,batch_size,input_size)
+labels=torch.LongTensor(y_data).view(batch_size,hidden_size)
+
